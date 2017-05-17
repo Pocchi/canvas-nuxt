cd728a21c7d84f0536a88aab630c0a5a444c2b8a
pocchi <Pocchi@users.noreply.github.com>
2017-05-18 00:32:26 +0900

    createCanvasを追加

---
+++ components/CanvasArea.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="CanvasArea">
+    <canvas id="tutorial"></canvas>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CanvasArea',
+  mounted: function () {
+    console.log('created')
+    this.$store.dispatch('createFunction')
+  }
+}
+</script>
+<style>
+</style>
